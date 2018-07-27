b8bdd85ce9a2703811754b2b295c75b0ebf857ec
myersb89 <myersb89@gmail.com>
2018-07-27 10:35:12 -0500

    updating site.py and __init__

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
+wsgi_app = app.wsgi_app
 from app import routes
--- site.py
+++ site.py
@@ -1,1 +1,4 @@
-from app import app
+from site import app
+
+if __name__ = '__main__:
+	app.run()
